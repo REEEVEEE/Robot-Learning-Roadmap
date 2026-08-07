df8d7565258c013fbead1713e6fab9af5343c595
REEEVEEE <973862215@qq.com>
2026-08-08 01:24:35 +0800

    Modify Motor class

--- Cpp/Motor.cpp
+++ Cpp/Motor.cpp
@@ -4,17 +4,30 @@ class Motor {
 
 private:
     float speed;
+     const int id;
 
 public:
 
-    void set_speed(float speed)
+    Motor(int id)
+        :speed(0),
+         id(id)
+    {
+
+    }
+
+    void setSpeed(float speed)
     {
         this->speed = speed;
     }
 
-    float get_speed()
+    float getSpeed() const
     {
         return speed;
+    }
+
+    int getID() const
+    {
+        return id;
     }
 
 };
@@ -22,11 +35,13 @@ public:
 
 int main(void)
 {
-    Motor motor_1;
-    motor_1.set_speed(100);
+    Motor motor_1(1);
+    motor_1.setSpeed(100);
 
-    float speed = motor_1.get_speed();
+    float speed = motor_1.getSpeed();
+    int id = motor_1.getID();
     std::cout << speed << std::endl;
+    std::cout << id << std::endl;
 
     return 0;
 }
